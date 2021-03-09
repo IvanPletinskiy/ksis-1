f96666bc7bd94216af1fd4d93c73473acf91b704
IvanPletinskiy <Ivan.Pliatsinski@effective-soft.com>
2021-03-09 23:59:54 +0300

    Implement arp

--- src/com/handen/ksis/Main.kt
+++ src/com/handen/ksis/Main.kt
@@ -1,16 +1,15 @@
 package com.handen.ksis
 
-import java.net.InetAddress
 import java.net.NetworkInterface
 
 @ExperimentalUnsignedTypes
 fun main() {
     val networkInterfaces = NetworkInterface.getNetworkInterfaces().toList()
-    val filteredNetforkInterfaces = networkInterfaces.filter {
+    val filteredNetworkInterfaces = networkInterfaces.filter {
         it.hardwareAddress != null && it.inetAddresses.toList().isNotEmpty()
     }
-    println("Total network interfaces:${filteredNetforkInterfaces.size}")
-    filteredNetforkInterfaces.forEach {
+    println("Total network interfaces:${filteredNetworkInterfaces.size}")
+    filteredNetworkInterfaces.forEach {
         println("=========================")
         val macAddressString = it.hardwareAddress.joinToString(".") { byte ->
             byte.toUByte().toString(16).toUpperCase()
@@ -21,6 +20,20 @@ fun main() {
         val inetAddresses = it.inetAddresses.toList()
         inetAddresses.forEach { inetAddress ->
             println("IP address:${inetAddress.hostAddress}")
+        }
+    }
+
+    println()
+    println()
+    println()
+    println("Network nodes:")
+
+    val process = Runtime.getRuntime().exec("arp -a")
+    val lines = process.inputStream.bufferedReader(charset("cp866")).lineSequence()
+    lines.forEach { line ->
+        if (line.contains("динамический")) {
+            val array = line.trim().split("\\s+".toRegex())
+            println("Network node IP address:${array[0]}, MacAddress:${array[1]}")
         }
     }
 }
